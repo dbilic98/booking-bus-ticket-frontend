42762bc481cc6068d91ca934e33fc395200c4063
dbilic98 <doris.bilic@student.fsre.ba>
2024-06-24 17:14:49 +0200

    Improve DateRangePicker layout for better responsiveness

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -19,45 +19,39 @@ const DateRangePicker: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center">
-      <div className="relative w-36">
-        {" "}
+    <div className="flex flex-col md:flex-row items-center md:space-x-4">
+      <div className="relative mb-2 md:mb-0 md:mr-2 w-full md:w-36">
         <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
+          ></svg>
         </div>
         <input
           name="start"
           type="date"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-8 pr-2 py-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+          className="w-full p-3 border rounded bg-gray-50 border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
           placeholder="DEPARTURE"
           onChange={handleStartDateChange}
           defaultValue={getCurrentDate()}
         />
       </div>
-      <span className="mx-5 text-gray-500">to</span>
-      <div className="relative w-36">
-        {" "}
+      <span className="mx-0 md:mx-5 my-2 md:my-0 text-gray-500">to</span>
+      <div className="relative mb-2 md:mb-0 w-full md:w-36">
         <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
-          >
-            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-          </svg>
+          ></svg>
         </div>
         <input
           name="end"
           type="date"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-8 pr-2 py-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+          className="w-full p-3 border rounded bg-gray-50 border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
           placeholder="RETURN"
           onChange={handleEndDateChange}
           defaultValue={getCurrentDate()}
