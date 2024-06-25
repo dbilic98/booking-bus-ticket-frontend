5d6342c33ac135cb672d3a0f149d1abf2507cb62
dbilic98 <doris.bilic@student.fsre.ba>
2024-06-25 17:21:23 +0200

    Add placesSlice reducer to store configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counterSlice";
 import dateRangeSlice from "../features/dateRangeSlice";
+import placesSlice from "../features/placeSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     dateRang: dateRangeSlice,
+    places: placesSlice,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export default store;
