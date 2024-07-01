0728ee56bea44480b2ca3b8c0c0d2e37c4e23b91
dbilic98 <doris.bilic@student.fsre.ba>
2024-07-01 21:28:16 +0200

    Added radio buttons for selecting roundtrip or oneway trip types

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CounterForm from "../components/CounterForm";
-import DateRangePicker from "../components/DateRangePicker";
+import DateRangePickerRoundtrip from "../components/DateRangePickerRoundtrip";
+import DateRangePickerOneWayRoute from "./DateRangePickerOneWay";
 import PlacesSearch from "../components/PlacesSearch";
 
 const SearchForm: React.FC<{
@@ -8,6 +9,7 @@ const SearchForm: React.FC<{
   setCurrentDate: (date: string) => void;
 }> = ({ currentDate, setCurrentDate }) => {
   const [showCounterForm, setShowCounterForm] = useState(false);
+  const [tripType, setTripType] = useState("roundtrip");
 
   const toggleCounterForm = () => {
     setShowCounterForm(!showCounterForm);
@@ -15,13 +17,63 @@ const SearchForm: React.FC<{
 
   return (
     <div className="bg-cream p-10 shadow-lg rounded-md w-full max-w-md">
+      <div className="flex justify-around mb-8 mr-48">
+        <label className="flex items-center space-x-2 cursor-pointer text-xs">
+          <input
+            type="radio"
+            name="tripType"
+            value="roundtrip"
+            checked={tripType === "roundtrip"}
+            onChange={() => setTripType("roundtrip")}
+            className="hidden"
+          />
+          <span
+            className={`p-2 rounded-full border-2 ${
+              tripType === "roundtrip"
+                ? "border-light-green bg-light-green"
+                : "border-gray-200 bg-white"
+            }`}
+          >
+            {tripType === "roundtrip" && (
+              <span className=" bg-light-green rounded-full"></span>
+            )}
+          </span>
+          <span>Roundtrip</span>
+        </label>
+        <label className="flex items-center space-x-2 cursor-pointer text-xs">
+          <input
+            type="radio"
+            name="tripType"
+            value="oneway"
+            checked={tripType === "oneway"}
+            onChange={() => setTripType("oneway")}
+            className="hidden"
+          />
+          <span
+            className={`p-2 rounded-full border-2 ${
+              tripType === "oneway"
+                ? "border-light-green bg-light-green"
+                : "border-gray-200 bg-white"
+            } `}
+          >
+            {tripType === "oneway" && (
+              <span className=" bg-light-green rounded-full"></span>
+            )}
+          </span>
+          <span>One way</span>
+        </label>
+      </div>
+
       <form className="space-y-6">
         <div className="flex space-x-4">
           <PlacesSearch />
         </div>
+
         <div className="w-full p-3 border rounded">
-          <DateRangePicker />
+          {tripType === "roundtrip" && <DateRangePickerRoundtrip />}
+          {tripType === "oneway" && <DateRangePickerOneWayRoute />}
         </div>
+
         <div className="w-full p-3 border rounded">
           <button
             type="button"
