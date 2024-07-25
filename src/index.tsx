9398e34b3e775a5c2c6da2d45980ca198a465b21
dbilic98 <doris.bilic@student.fsre.ba>
2024-07-25 17:44:06 +0200

    Added Routes and Route from react-router-dom to set up routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import store from "./redux/store";
 import App from "./App";
+import RoutesView from "./components/RoutesView";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -12,9 +13,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/route-view" element={<RoutesView />} />
+      </Routes>
     </Router>
   </Provider>
 );
