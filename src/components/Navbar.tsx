73ed023385f2b30dec76fadd738685222ca4e0f6
dbilic98 <doris.bilic@student.fsre.ba>
2024-09-21 13:48:51 +0200

    Update header navigation links and integrate LogOut component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LogOut from "../auth/Logout";
 
 const Header: React.FC = () => {
   return (
@@ -6,19 +7,19 @@ const Header: React.FC = () => {
       <h1 className="text-xl font-bold pl-8">BMS</h1>
 
       <nav className="flex-1 ml-[20%] flex space-x-20">
-        <a href="/" className="hover:underline">
+        <a href="/home" className="hover:underline">
           HOME
         </a>
-        <a href="#" className="hover:underline">
+        <a href="/mybooking" className="hover:underline">
           MY BOOKING
         </a>
-        <a href="#" className="hover:underline">
+        <a href="/about" className="hover:underline">
           ABOUT
         </a>
       </nav>
       <nav className="bg-jet-black text-white p-4 flex basis-2/5 ">
-        <a href="#" className="flex-1 ml-[80%]">
-          LOG OUT
+        <a href="/login" className="flex-1 ml-[80%]">
+          <LogOut />
         </a>
       </nav>
     </header>
